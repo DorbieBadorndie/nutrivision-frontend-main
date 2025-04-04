5e7dbe6cda48d466c4ac90f4e38ca07a070cf90c
Lei Aguila <aguila.christleidaniel@gmail.com>
2025-04-05 00:25:41 +0800

    update on feedback screen

--- app/feedback.tsx
+++ app/feedback.tsx
@@ -1,4 +1,5 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { useState } from 'react';
+import { View, Text, StyleSheet, Image } from 'react-native';
 
 const styles = StyleSheet.create({
     container: {
@@ -6,20 +7,55 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
     },
+    sugarContainer: {
+        width: 380,
+        height: 94,
+        borderRadius: 20,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#FFFBFB',
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.1,
+        shadowRadius: 6,
+    },
+    image: {
+        position: 'absolute',
+        top: -15,
+        left: -7,
+        width: 200,
+        height: 200,
+        resizeMode: 'contain',
+      },
 })
 
-function Sugar() {
+function Logo() {
     return(
-        <View style={styles.container}>
-            <Text>Sugar</Text>
+        <View>
+            <Image 
+                source={require('@/assets/images/NutriVision.png')}
+                style={styles.image}
+                accessibilityRole="image"
+                accessibilityLabel="NutriVision logo"
+            />
+        </View>
+    );
+}
+
+function Sugar() {
+    const [sugar, setSugar] = useState(0);
+    return(
+        <View style={styles.sugarContainer}>
+            <Text>Sugar: {sugar}</Text> 
+
         </View>
     )
 }
 
-function Feedback() {
+function Feedback() { 
     return(
         <View style={styles.container}>
-            <Text>Feedback</Text>
+            <Sugar />
         </View>
     )
 }

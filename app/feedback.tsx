6bf53e6bbd62e2cc6dcdbc03ea2807e51005192f
Lei Aguila <aguila.christleidaniel@gmail.com>
2025-04-05 01:54:46 +0800

    new changes feedback

--- app/feedback.tsx
+++ app/feedback.tsx
@@ -1,5 +1,8 @@
 import { useState } from 'react';
-import { View, Text, StyleSheet, Image } from 'react-native';
+import { View, Text, StyleSheet, Image, Dimensions, SafeAreaView } from 'react-native';
+import { ThemedView } from '@/components/ThemedView';
+
+const { width: SCREEN_WIDTH } = Dimensions.get('window');
 
 const styles = StyleSheet.create({
     container: {
@@ -8,8 +11,8 @@ const styles = StyleSheet.create({
         alignItems: 'center',
     },
     sugarContainer: {
-        width: 380,
-        height: 94,
+        width: SCREEN_WIDTH * 0.9,
+        paddingVertical: 20,
         borderRadius: 20,
         justifyContent: 'center',
         alignItems: 'center',
@@ -19,20 +22,40 @@ const styles = StyleSheet.create({
         shadowOpacity: 0.1,
         shadowRadius: 6,
     },
-    image: {
+    logoContainer: {
         position: 'absolute',
-        top: -15,
-        left: -7,
-        width: 200,
-        height: 200,
+        top: -65,
+        left: 0,
+    },
+    image: {
+        width: SCREEN_WIDTH * 0.5,
+        height: SCREEN_WIDTH * 0.5,
         resizeMode: 'contain',
-      },
-})
+    },
+    textContainer: {
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        width: '100%',
+        paddingHorizontal: 20,
+    },
+    textHeader: {
+        fontFamily: 'AlberSans-Regular',
+        fontWeight: 'bold',
+        fontSize: 18,
+        color: '#000',
+        marginBottom: 5,
+    },
+    textSub: {
+        fontSize: 16,
+        color: '#333',
+        marginBottom: 3,
+    }
+});
 
 function Logo() {
-    return(
-        <View>
-            <Image 
+    return (
+        <View style={styles.logoContainer}>
+            <Image
                 source={require('@/assets/images/NutriVision.png')}
                 style={styles.image}
                 accessibilityRole="image"
@@ -43,21 +66,38 @@ function Logo() {
 }
 
 function Sugar() {
-    const [sugar, setSugar] = useState(0);
-    return(
+    const [sugar, setSugar] = useState<number>(0);
+    const [approx, setApprox] = useState<number>(0);
+    const [tablespoon, setTablespoon] = useState<number>(0);
+    
+    return (
         <View style={styles.sugarContainer}>
-            <Text>Sugar: {sugar}</Text> 
-
+            <View style={styles.textContainer}>
+                <Text style={styles.textHeader}>
+                    Sugar: {sugar} g
+                </Text>
+                <Text style={styles.textSub}>
+                    Approx. {approx} grams
+                </Text>
+                <Text style={styles.textSub}>
+                    Equivalent {tablespoon} tablespoons
+                </Text>
+            </View>
         </View>
-    )
+    );
 }
 
-function Feedback() { 
-    return(
-        <View style={styles.container}>
-            <Sugar />
-        </View>
-    )
+function Feedback() {
+    return (
+        <SafeAreaView style={{ flex: 1 }}>
+            <View style={{flex: 1, paddingHorizontal: 20}}>
+                <ThemedView style={[styles.container, { backgroundColor: '#eff1f6' }]}>
+                    <Logo />
+                    <Sugar />
+                </ThemedView>
+            </View>
+        </SafeAreaView>
+    );
 }
 
 export default Feedback;
